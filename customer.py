6d7273c0d2cbff43afb4be891a2f2de8f4bb04b1
Faridah <faridahdada@outlook.com>
2021-03-08 18:52:15 +0000

    fixed error in code

--- customer.py
+++ customer.py
@@ -1,7 +1,7 @@
-from person import
+from person import Person
 class Customer(Person):
     def __init__(self, first_name, last_name, age, customer_id, membership_type):
-        super()__init__(first_name, last_name, age)
+        super().__init__(first_name, last_name, age)
         self.customer_id = customer_id
         self.membership_type = membership_type
 
